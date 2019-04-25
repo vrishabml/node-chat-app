fff8da31d8bd9f5db0a30a094aad2e445f99339f
Vrishab ML <vrishabm0@gmail.com>
2019-04-25 11:37:48 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,21 +14,35 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('new user connected');
 
-  
+  socket.emit('newMessage', {
+    from:'Admin',
+    text:'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from:'Admin',
+    text:'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
-    console.log('createEmail', message);
+    console.log('createMessage', message);
     io.emit('newMessage',{
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
-  })
+    });
+});
+  //   socket.broadcast.emit('newMessage', {
+  //     from: message.from,
+  //     text: message.text,
+  //      createdAt: new Date().getTime()})
+  // });
   socket.on('disconnect', (socket) => {
     console.log('Client Disconnected');
   });
-
 });
+
 
 
 server.listen(port, () => {
