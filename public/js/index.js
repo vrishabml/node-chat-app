7f98168220e791014509ab1e0bc42d37ba520933
Vrishab ML <vrishabm0@gmail.com>
2019-04-28 12:14:44 +0530

    Add  form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,25 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    toAddress:'vrishab@facebook.com',
-    text:'How you doing?'
-  })
+  
 });
 socket.on('newMessage',function(message) {
   console.log('New Message received', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from} : ${message.text}`);
+  jQuery('#messages').append(li);
 });
 socket.on('disconnect', function()  {
   console.log('Disconnected from server');
+});
+
+jQuery('#message-form').on('submit',function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from:'User',
+    text:jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
